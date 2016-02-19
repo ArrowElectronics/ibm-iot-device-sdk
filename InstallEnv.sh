164593d98906421b4d9afc981e42d917e75ab961
rschwarz-arrow <rschwarz@arrow.com>
2016-02-19 16:26:34 +0000

    Updated installer script

--- InstallEnv.sh
+++ InstallEnv.sh
@@ -74,7 +74,7 @@ sudo addgroup linaro i2c  # Allow the normal user to perform i2c operations
 git clone https://github.com/96boards/96boards-tools
 sudo cp 96boards-tools/70-96boards-common.rules /etc/udev/rules.d/
 
-cat | sudo tee /etc/profile.d/96boards-sensors.sh << EOF export JAVA_TOOL_OPTIONS="-Dgnu.io.rxtx.SerialPorts=/dev/tty96B0" export MONITOR_PORT=/dev/tty96B0 export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python2.7/site-packages" EOF
+echo 'export JAVA_TOOL_OPTIONS="-Dgnu.io.rxtx.SerialPorts=/dev/tty96B0" export MONITOR_PORT=/dev/tty96B0 export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python2.7/site-packages"' | sudo tee /etc/profile.d/96boards-sensors.sh
 sudo cp /etc/profile.d/96boards-sensors.sh /etc/X11/Xsession.d/96boards-sensors
 
 #Build the demo
@@ -89,4 +89,4 @@ cp ~/Projects/ibm-iot-starter-kit/db410c_demo.desktop ~/Desktop
 #Reboot the system:
 #-------------------------------------------------------------------------------
 #Now reboot the system to pick up all the changes.
-sudo reboot
+#sudo reboot
