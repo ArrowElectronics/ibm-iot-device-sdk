e67fd5aeec9e95435ae800a663382a08e6420df6
rschwarz-arrow <rschwarz@arrow.com>
2016-02-19 16:11:07 +0000

    Added desktop shortcut to demo

--- InstallEnv.sh
+++ InstallEnv.sh
@@ -77,6 +77,15 @@ sudo cp 96boards-tools/70-96boards-common.rules /etc/udev/rules.d/
 cat | sudo tee /etc/profile.d/96boards-sensors.sh << EOF export JAVA_TOOL_OPTIONS="-Dgnu.io.rxtx.SerialPorts=/dev/tty96B0" export MONITOR_PORT=/dev/tty96B0 export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python2.7/site-packages" EOF
 sudo cp /etc/profile.d/96boards-sensors.sh /etc/X11/Xsession.d/96boards-sensors
 
+#Build the demo
+#-------------------------------------------------------------------------------
+cd ~/Projects/ibm-iot-starter-kit/demo
+make ibm_demo
+
+#Create a shortcut on the desktop
+#-------------------------------------------------------------------------------
+cp ~/Projects/ibm-iot-starter-kit/db410c_demo.desktop ~/Desktop
+
 #Reboot the system:
 #-------------------------------------------------------------------------------
 #Now reboot the system to pick up all the changes.
